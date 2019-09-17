cc3c297892cc59a018f3a23db70495aaddaa19f1
Anindya Basu <aiwabdn@Anindyas-MacBook-Pro.local>
2019-09-17 08:34:32 +0100

    chapter 8: adding followers

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import os
 from config import Config
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
--- app/models.py
+++ app/models.py
@@ -5,14 +5,28 @@ from hashlib import md5
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
+followers = db.Table('followers',
+                    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+                    db.Column('followed_id', db.Integer, db.ForeignKey('user.id')))
+
+# class Followers(db.Model):
+#     follower_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+#     followed_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    followed = db.relationship('User', secondary=followers,
+                               primaryjoin=followers.c.follower_id == id,
+                               secondaryjoin=followers.c.followed_id == id,
+                               backref=db.backref('followers', lazy='dynamic'),
+                               lazy='dynamic')
 
     def __repr__(self):
         return '<User: {}>'.format(self.username)
@@ -28,6 +42,16 @@ class User(db.Model, UserMixin):
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
 
+    def is_following(self, user):
+        return self.followed.filter(followers.c.follower_id == user.id).count() > 0
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
